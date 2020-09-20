bce48192f887f7434acd82460e0ba509ce455630
Elisangela L <elimacxx@gmail.com>
2020-09-20 14:45:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const text = 'Pense, fale, compre, beba\nLeia, vote, não se esqueça\nUse, seja, ouça, diga\nTenha, more, gaste e viva  \n"Pitty Leone"    '
+
+let index = 0;
+
+function escreveFrase(){
+    document.getElementById('texto').innerText = text.slice(0, index)
+    
+    index++;
+    if(index > text.length-1){
+        index=0;
+    }
+}
+
+setInterval(escreveFrase,100)
+
+console.log('oi')
